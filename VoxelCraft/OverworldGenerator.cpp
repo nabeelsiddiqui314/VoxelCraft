293afbbb752cdcbdec0ab3e1258c37bfdabfc88e
Nabeel Siddiqui <dninja797@gmail.com>
2019-10-04 19:08:37 +0530

    made world generation faster by removing an unnecessary for loop

--- VoxelCraft/OverworldGenerator.cpp
+++ VoxelCraft/OverworldGenerator.cpp
@@ -9,36 +9,36 @@ OverworldGenerator::OverworldGenerator()
 
 Chunks OverworldGenerator::generateChunk(const VecXZ& pos) {
 	Chunks chunk;
-
+	sf::Clock c;
 	std::int16_t height;
+    m_biomeVal = m_biomeNoise.getNoiseAt(pos.x * Segment::WIDTH, pos.z * Segment::WIDTH);
 
-	for (std::int16_t x = 0; x < Segment::WIDTH; x++) {
-		for (std::int16_t z = 0; z < Segment::WIDTH; z++) {
-			m_biomeVal = m_biomeNoise.getNoiseAt(x + pos.x * Segment::WIDTH, z + pos.z * Segment::WIDTH);
-			height = getCurrentBiome().getHeightAt(x + pos.x * Segment::WIDTH, z + pos.z * Segment::WIDTH);
+	for (std::int16_t x = 0; x < Segment::WIDTH; x++) 
+	for (std::int16_t z = 0; z < Segment::WIDTH; z++) {
+		height = getCurrentBiome().getHeightAt(x + pos.x * Segment::WIDTH, z + pos.z * Segment::WIDTH);
+		int depth = 0;
 
-			int depth = 0;
-			for (int y = height; y-- > 0;) {
+		for (int y = Chunks::HEIGHT * Segment::WIDTH; y-- > 0;) {
+			if (y <= height) {
 				chunk.setBlock(x, y, z, getCurrentBiome().getComposition().getBlockAt(depth));
 				depth++;
 			}
 			if (getCurrentBiome().hasWater()) {
-				for (std::int16_t y = 0; y < Chunks::HEIGHT * Segment::WIDTH; y++) {
-					if (y <= Chunks::HEIGHT * Segment::WIDTH / 4) {
-						switch (BlockCodex::getBlockData(chunk.getBlock(x, y, z)).category) {
-						case BlockCategory::NONE:
-							if (y <= Chunks::HEIGHT * Segment::WIDTH / 4 - 3)
-								chunk.setBlock(x, y, z, BlockType::WATER);
-							break;
-						case BlockCategory::SOLID:
-							chunk.setBlock(x, y, z, BlockType::SAND);
-							break;
-						}
+				if (y <= Chunks::HEIGHT * Segment::WIDTH / 4) {
+					switch (BlockCodex::getBlockData(chunk.getBlock(x, y, z)).category) {
+					case BlockCategory::NONE:
+						if (y <= Chunks::HEIGHT * Segment::WIDTH / 4 - 3)
+							chunk.setBlock(x, y, z, BlockType::WATER);
+						break;
+					case BlockCategory::SOLID:
+						chunk.setBlock(x, y, z, BlockType::SAND);
+						break;
 					}
 				}
 			}
 		}
 	}
+	std::cout << c.getElapsedTime().asMilliseconds() << "\n";
 	return chunk;
 }
 

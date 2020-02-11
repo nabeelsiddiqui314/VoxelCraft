e4473615eb36f0d76e91bd864fe9ec8fa6d7f7d3
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-02-12 01:22:58 +0530

    thread pool: renamed queue and more efficient pop.

--- VoxelCraft/Blocking_Queue.h
+++ VoxelCraft/Blocking_Queue.h
@@ -2,28 +2,30 @@
 #include <queue>
 #include <mutex>
 
-template <typename T>
-class Safe_Queue {
+template <typename T, std::size_t Capacity>
+class Blocking_Queue {
 public:
-	T pop() {
+	void pop(T& popped_data) {
 		std::unique_lock<std::mutex> lock(m_mutex);
+
 		while (m_queue.empty()) {
-			m_blocker.wait(lock);
+			m_consumers.wait(lock);
 		}
 
-		auto data = m_queue.front();
+		popped_data = m_queue.front();
 		m_queue.pop();
-		return data;
 	}
 
 	void push(const T& data) {
 		std::unique_lock<std::mutex> lock(m_mutex);
+
 		m_queue.push(data);
+
 		lock.unlock();
-		m_blocker.notify_one();
+		m_consumers.notify_one();
 	}
 private:
 	std::queue<T> m_queue;
 	std::mutex m_mutex;
-	std::condition_variable m_blocker;
+	std::condition_variable m_consumers;
 };
